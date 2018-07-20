1965e9796d2ad6c53bb5c86467c66fa4b4ef5f10
jashanbhullar <jsbhullar96@gmail.com>
2018-07-20 22:06:52 +0530

    Create Product Form Complete

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../category.service';
 import { ProductService } from '../../product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -11,6 +12,7 @@ export class ProductFormComponent implements OnInit {
 
   categories$;
   constructor(
+    private router: Router,
     private categoryService: CategoryService,
     private productService: ProductService,
   ) { }
@@ -21,6 +23,7 @@ export class ProductFormComponent implements OnInit {
 
   save(value) {
     this.productService.create(value);
+    this.router.navigateByUrl('/admin/products');
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { CustomFormsModule } from 'ng2-validation';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
@@ -48,6 +49,7 @@ import { ProductService } from './product.service';
     AngularFireAuthModule,
     AngularFireDatabaseModule,
     FormsModule,
+    CustomFormsModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([
       {path: '', component: HomeComponent},
